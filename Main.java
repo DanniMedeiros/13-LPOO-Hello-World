31cfb4d857eaa60a3341ca6de329f3d2b09e8568
DanniMedeiros <95725544+DanniMedeiros@users.noreply.github.com>
2023-08-31 22:29:34 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Danielly Nakano Medeiros"); // Prof, você é bem didático!
+      System.out.println("Hello World!");
+  }
+}
